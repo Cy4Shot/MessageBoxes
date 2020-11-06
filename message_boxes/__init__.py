0adc95d6299e6e3094c5ba360e13b748335cce22
Cy4Shot <veschi@gmail.com>
2020-11-06 18:02:23 +0000

    Faster Message Boxes

--- message_boxes/__init__.py
+++ message_boxes/__init__.py
@@ -13,18 +13,9 @@ wrapper = {
 }
 
 def message_box(title, content, args=''):
-    """Creats a message box. WINDOWS USERS ONLY!
-
-    Parameters:
-    title (string): Title of the message box.
-    content (string): Content of the message box.
-    args (string): VB arguments for message box (DEFAULT = '').
-    """
-    if platform.system() == 'Windows':
+    if platform.system()=='Windows':
         with TemporaryDirectory() as temp_dir:
-            with open(temp_dir + '\\data.vbs', 'w') as f:
-                f.write('x=MsgBox("'+content+'", '+str(args)+', "'+title + '")\nSet fso = CreateObject ("Scripting.FileSystemObject")\nSet stdout = fso.GetStandardStream (1)\nstdout.WriteLine x')
-            result = run('wscript //nologo ' + temp_dir + '\\data.vbs', stdout=PIPE, stderr=PIPE, universal_newlines=True)
-            return wrapper[int(result.stdout)]
+            open(temp_dir+'\\data.vbs','w').write('x=MsgBox("'+content+'",'+str(args)+',"'+title+'")\nSet fso=CreateObject("Scripting.FileSystemObject")\nSet stdout=fso.GetStandardStream(1)\nstdout.WriteLine x')
+            return wrapper[int(run('wscript //nologo '+temp_dir+'\\data.vbs',stdout=PIPE).stdout)]
     else:
         raise(OSError("Wrong Operating System: Must be Windows"))
